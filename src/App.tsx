a2625b9d5b7d03ecf0ea2a03913318767265ad07
Joshua Hickey <joshuah775@gmail.com>
2025-06-03 21:44:40 +0100

    Add time button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 
+import { Timer } from 'lucide-react'
+import Button from './components/Button'
 import FilterBar from './components/FilterBar'
 import Header from './components/Header'
 import Trackbar from './components/Trackbar'
@@ -20,6 +22,10 @@ function App() {
     <div id="content" className='container  flex-grow flex flex-col items-center py-6 space-y-6 lg:px-0 px-4'>
         <Trackbar />
         <FilterBar />
+        <Button className="w-full bg-gradient-to-r from-red-500 to-pink-500 text-white !justify-center !text-center font-bold py-3 px-6 !rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+            <Timer />
+            Add Lap Time
+        </Button>
     </div>
     </>
     )
