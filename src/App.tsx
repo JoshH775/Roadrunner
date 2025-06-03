f7f5a0d8b0f7de1ac3fbbdbc238b604f1f7e5357
Joshua Hickey <joshuah775@gmail.com>
2025-06-03 11:12:29 +0100

    Fetching cars on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+
+import FilterBar from './components/Filters/FilterBar'
+import Header from './components/Header'
+import Trackbar from './components/Trackbar'
+import { useAppState } from './StateProvider'
+
+import { useEffect } from 'react'
 
 function App() {
 
+    const { fetchCars } = useAppState()
+
+    useEffect(() => {
+        fetchCars()
+    }, [fetchCars])
+
   return (
     <>
-rah
+    <Header />
+    <div id="content" className='container  flex-grow flex flex-col items-center py-6 space-y-6 lg:px-0 px-4'>
+        <Trackbar />
+        <FilterBar />
+    </div>
     </>
-  )
+    )
 }
 
 export default App
