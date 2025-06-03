90e5446a7a217e3ed7a50e13eb2ced98bcec33c4
Joshua Hickey <joshuah775@gmail.com>
2025-06-03 11:16:43 +0100

    Creatign basic header

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { PersonStanding } from "lucide-react";
+import Button from "./Button";
+
+export default function Header() {
+  return (
+    <header className="w-full border-b flex items-center justify-center border-gray-300 py-5 shadow-lg">
+      <div className="container flex gap-2 items-center justify-between lg:p-0 px-6">
+        <p className="lg:text-4xl text-2xl font-bold italic">Roadrunner</p>
+        <Button>
+          <PersonStanding />
+          Manage Friends
+        </Button>
+      </div>
+    </header>
+  );
+}
