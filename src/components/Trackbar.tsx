87978ce989ce7565b944b0726d9e2a38fbddbd3a
Joshua Hickey <joshuah775@gmail.com>
2025-06-07 19:25:23 +0100

    idk

--- src/components/Trackbar.tsx
+++ src/components/Trackbar.tsx
@@ -9,7 +9,7 @@ export default function Trackbar() {
 
     return (
         <div id="track-component" className='w-full flex lg:flex-row flex-col shadow-2xl p-4 lg:p-6 gap-2 lg:gap-0 lg:justify-between items-center rounded-lg bg-gradient-to-r from-blue-500/13 to-purple-500/13'>
-            <div className="flex lg:gap-6 gap-3 items-center min-w-fit w-full lg:w-auto">
+            <div className="flex lg:gap-6 gap-3 items-center min-w-fit ">
             <Badge icon={<MapPin className='text-white'/>} className='bg-gradient-to-r from-red-500 to-pink-500 !w-16 !h-16'/>
             <div className='space-y-2'>
                 <p className=' lg:text-2xl text-xl font-bold'>{selectedTrack.name}</p>
