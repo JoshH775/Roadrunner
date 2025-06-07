6ec7c944dbeb48b76f94b4ae9ce848fb81ae0204
Joshua Hickey <joshuah775@gmail.com>
2025-06-07 19:25:37 +0100

    logout button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
 import { PersonStanding } from "lucide-react";
 import Button from "./UI/Button";
+import { supabase } from "../supabase";
+import { useAppState } from "../StateProvider";
 
 export default function Header() {
+
+  const { logout } = useAppState();
+
   return (
     <header className="w-full border-b flex items-center justify-center border-gray-300 py-5 shadow-lg">
       <div className="container flex gap-2 items-center justify-between lg:p-0 px-6">
@@ -9,6 +14,9 @@ export default function Header() {
         <Button>
           <PersonStanding />
           Manage Friends
+        </Button>
+        <Button onClick={logout}>
+          Logout
         </Button>
       </div>
     </header>
