7bb79e89fe8304208ce8c587002b0c6d73debac5
Joshua Hickey <joshuah775@gmail.com>
2025-06-08 15:50:53 +0100

    Adding lap time inputs

--- src/components/Modals/AddTimeModal.tsx
+++ src/components/Modals/AddTimeModal.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Modal from "../UI/Modal";
 import {
     type Car,
   type LapTime,
-  type ModificationOption,
   type Track,
 } from "../../../types";
 import TrackCombobox from "../TrackCombobox";
 import Button from "../UI/Button";
-import { Calendar, CarIcon, ChevronsUpDown, DecimalsArrowRight, Route } from "lucide-react";
+import { Calendar, CarIcon, ChevronsUpDown, DecimalsArrowRight, Route, Timer } from "lucide-react";
 import CarCombobox from "../CarCombobox";
 import { useAppState } from "../../StateProvider";
-import moment from "moment";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+dayjs.extend(duration);
 
 export default function AddTimeModal({
   isOpen,
@@ -30,51 +31,17 @@ export default function AddTimeModal({
     pi: 100,
     carId: car?.id || 0,
     trackId: track.id,
-    date: moment().unix(),
+    date: dayjs().unix(),
     userId: user?.id || 0,
     engineSwap: false,
     drivetrainSwap: false,
     flyingLap: false,
   });
 
-  const onModSelect = (modification: ModificationOption) => {
-    switch (modification) {
-      case "engine":
-        setLapTime((prev) => ({
-          ...prev,
-          engineSwap: true,
-          drivetrainSwap: false,
-        }));
-        break;
-      case "drivetrain":
-        setLapTime((prev) => ({
-          ...prev,
-          engineSwap: false,
-          drivetrainSwap: true,
-        }));
-        break;
-      case "both":
-        setLapTime((prev) => ({
-          ...prev,
-          engineSwap: true,
-          drivetrainSwap: true,
-        }));
-        break;
-      case "stock":
-        setLapTime((prev) => ({
-          ...prev,
-          engineSwap: false,
-          drivetrainSwap: false,
-        }));
-        break;
-      default:
-        setLapTime((prev) => ({
-          ...prev,
-          engineSwap: false,
-          drivetrainSwap: false,
-        }));
-    }
-  };
+
+  const minutesRef = useRef<HTMLInputElement>(null);
+  const secondsRef = useRef<HTMLInputElement>(null);
+  const millisecondsRef = useRef<HTMLInputElement>(null);
 
   const onTrackSelect = (selectedTrack: Track) => {
     setTrack(selectedTrack);
@@ -85,6 +52,39 @@ export default function AddTimeModal({
     setCar(selectedCar);
     setLapTime((prev) => ({ ...prev, carId: selectedCar.id }));
   };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    minutesRef.current?.setCustomValidity("");
+    
+    const minutes = Number(minutesRef.current?.value) || 0;
+    const seconds = Number(secondsRef.current?.value) || 0;
+    const milliseconds = Number(millisecondsRef.current?.value) || 0;
+
+    console.log("Lap Time Input:", {
+      minutes,
+      seconds,
+      milliseconds,
+    });
+
+    if (minutes === 0 && seconds === 0) {
+      console.log('here')
+      minutesRef.current?.setCustomValidity("Minutes and seconds cannot both be zero.");
+      minutesRef.current?.reportValidity();
+      return;
+    } 
+
+    const totalMilliseconds = (minutes * 60 + seconds) * 1000 + milliseconds;
+
+    const fullLapTime = {
+      ...lapTime,
+      time: totalMilliseconds,
+    };
+
+    console.log("Submitting lap time:", fullLapTime);
+    onClose();
+  }
 
   return (
     <Modal
@@ -107,7 +107,7 @@ export default function AddTimeModal({
       ]}
     >
       <p className="text-gray-600 mb-2">Record your latest lap time here</p>
-      <form id="add-lap-time-form" onSubmit={(e) => {console.log("Form submitted", e); e.preventDefault(); onClose();}}>
+      <form id="add-lap-time-form" onSubmit={onSubmit}>
 
         <div className="grid grid-cols-2 grid-rows-2 lg:gap-4 gap-2 place-items-center">
         <div className="w-full h-full flex flex-col items-between col-span-2 lg:col-span-1">
@@ -154,12 +154,12 @@ export default function AddTimeModal({
           <label className="font-semibold mb-1 flex text-sm"><Calendar className="w-4 mr-2" />Date</label>
           <input
             type="date"
-            value={moment.unix(lapTime.date).format("YYYY-MM-DD")}
+            value={dayjs.unix(lapTime.date).format("YYYY-MM-DD")}
             required
             onChange={(e) =>
               setLapTime((prev) => ({
                 ...prev,
-                date: moment(e.target.value).unix(),
+                date: dayjs(e.target.value).unix(),
               }))
             }
             className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-red-500"
@@ -179,13 +179,47 @@ export default function AddTimeModal({
             value={lapTime.pi}
             min={100}
             max={999}
+            maxLength={3}
             required
             onChange={(e) =>
               setLapTime((prev) => ({ ...prev, pi: Number(e.target.value) }))
             }
             className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-red-500"
-            placeholder="Enter Performance Index"
+            placeholder="Enter Performance Index (PI)"
           />
+        </div>
+
+        <div className="w-full h-full flex flex-col items-between">
+          <label className="font-semibold mb-1 flex text-sm"><Timer className="w-4 mr-2" />Lap Time</label>
+          <div className="flex items-center space-x-2">
+            <input
+              type="number"
+              ref={minutesRef}
+              className="w-16 border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+              placeholder="MM"
+              required
+              min={0}
+            />
+            <span>:</span>
+            <input
+              type="number"
+              ref={secondsRef}
+              className="w-16 border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+              placeholder="SS"
+              required
+              min={0}
+            />
+            <span>:</span>
+            <input
+              type="number"
+              ref={millisecondsRef}
+              className="w-20 border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+              placeholder="Ms"
+              min={0}
+              max={999}
+            />
+          </div>
+              
         </div>
 
         </div>
