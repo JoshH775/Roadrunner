0ee3ddda9380f035c00c7859d5ed2f581bac06aa
Joshua Hickey <joshuah775@gmail.com>
2025-06-08 19:48:01 +0100

    toater

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
+  <>
+    <Toaster containerStyle={{ zIndex: 10000 }} />
     <App />
-)
+  </>
+);
