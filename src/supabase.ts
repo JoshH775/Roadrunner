2e6fbb51c87800cffadafa3f8f19270c647f9aec
Joshua Hickey <joshuah775@gmail.com>
2025-06-09 02:18:06 +0100

    adding functions to add lap times and friends

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import type { DBResponse, User } from "../types";
+import type { DBResponse, LapTime, User } from "../types";
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL || "";
 const supabasePublicKey = process.env.VITE_SUPABASE_PUBLIC_KEY || "";
@@ -137,4 +137,96 @@ function generateFriendCode(): string {
     code += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return code;
+}
+
+
+export async function addFriend(userId: number, friendCode: string): Promise<DBResponse<{ id: number; username: string, friendCode: string }>> {
+  return asyncWrapper(async () => {
+    const { data: userData, error: userError } = await supabase
+      .from("users")
+      .select("id")
+      .eq("friend_code", friendCode)
+      .single();
+
+    if (userError || !userData) {
+      return {
+        data: null,
+        error: "Friend code not found",
+      };
+    }
+
+    const { error: insertError } = await supabase
+      .from("user_friends")
+      .insert({
+        user_id: userId,
+        friend_id: userData.id,
+      });
+
+    if (insertError) {
+      return {
+        data: null,
+        error: insertError.message || "Failed to add friend",
+      };
+    }
+
+    const { data: friendData, error: fetchError } = await supabase
+      .from("users")
+      .select("id, username, friend_code")
+      .eq("id", userData.id)
+      .single();
+
+    if (fetchError || !friendData) {
+      return {
+        data: null,
+        error: fetchError?.message || "Failed to fetch friend data",
+      };
+    }
+
+    return {
+      data: {
+        id: friendData.id,
+        username: friendData.username,
+        friendCode: friendData.friend_code,
+      },
+    };
+
+    
+  });
+}
+
+export async function addLapTime(
+  lapTime: Omit<LapTime, "id" | "userId">,
+  userId: number
+) {
+  return asyncWrapper(async () => {
+    const lapTimeSnakeCase = {
+      user_id: userId,
+      car_id: lapTime.carId,
+      track_id: lapTime.trackId,
+      time: lapTime.time,
+      pi: lapTime.pi,
+      date: lapTime.date,
+      engine_swap: lapTime.engineSwap,
+      drivetrain_swap: lapTime.drivetrainSwap,
+      flying_lap: lapTime.flyingLap,
+    }
+
+    console.log("Adding lap time:", lapTimeSnakeCase);
+    const { data, error } = await supabase
+      .from("lap_times")
+      .insert(lapTimeSnakeCase)
+      .select("*")
+      .single();
+
+    if (error || !data) {
+      return {
+        data: null,
+        error: error?.message || "Failed to add lap time",
+      };
+    }
+
+    return {
+      data: data as LapTime,
+    };
+  });
 }
