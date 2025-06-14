bcffa47bb9d8bba039ae182ec948331065f1515c
Joshua Hickey <joshuah775@gmail.com>
2025-06-14 01:10:36 +0100

    Finished Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
-import { PersonStanding } from "lucide-react";
+import { LogOutIcon, Users } from "lucide-react";
 import Button from "./UI/Button";
 import { useAppState } from "../StateProvider";
 import SocialModal from "./Modals/SocialModal";
 import { useState } from "react";
+import Badge from "./UI/Badge";
+import { LeaderboardIcon } from "./UI/CustomIcons";
 
 export default function Header() {
 
@@ -10,17 +12,28 @@ export default function Header() {
   const [isSocialModalOpen, setIsSocialModalOpen] = useState(false);
 
   return (
-    <header className="w-full border-b flex items-center justify-center border-gray-300 py-5 shadow-lg">
+    <header className="w-full border-b flex items-center justify-center border-gray-300 py-2 shadow-lg">
       <SocialModal isOpen={isSocialModalOpen} onClose={() => setIsSocialModalOpen(false)} />
       <div className="container flex gap-2 items-center justify-between lg:p-0 px-6">
-        <p className="lg:text-4xl text-2xl font-bold italic">Roadrunner</p>
-        <Button onClick={() => setIsSocialModalOpen(true)}>
-          <PersonStanding />
+
+
+        <div className="flex items-center gap-2">
+          <Badge icon={<LeaderboardIcon className="fill-none text-white"/>} className="h-11 !p-1 bg-gradient-to-br from-red-500 to-pink-500" />
+          <span>
+              <p className="lg:text-2xl text-xl font-bold italic bg-gradient-to-r from-red-500 to-pink-500 bg-clip-text text-transparent">Roadrunner</p>
+              <p className="text-gray-500 font-semibold">Forza Horizon 5 Lap Time Tracker</p>
+          </span>
+        </div>
+
+        <div className="flex items-center gap-2 text-gray-800">
+                  <Button onClick={() => setIsSocialModalOpen(true)} icon={<Users className="w-4"/>}>
           Manage Friends
         </Button>
-        <Button onClick={logout}>
+        <Button onClick={logout} icon={<LogOutIcon className="w-4"/>}>
           Logout
         </Button>
+        </div>
+
       </div>
     </header>
   );
