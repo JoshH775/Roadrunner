7a20d418c9ded9925dd8a30c676d68d77ec6f342
Joshua Hickey <joshuah775@gmail.com>
2025-07-07 20:39:05 +0100

    Creating a global tab

--- src/components/UserTabs.tsx
+++ src/components/UserTabs.tsx
@@ -1,48 +1,76 @@
 import { useMemo } from "react";
-import { Trophy } from "lucide-react";
+import { Globe, Trophy } from "lucide-react";
 import type { Friend } from "../../types";
 import { useAppState } from "../StateProvider";
 import Button from "./UI/Button";
 
 export default function UserTabs() {
-    const { viewedUserId, setViewedUserId, user } = useAppState();
+  const { viewedUserId, setViewedUserId, user } = useAppState();
 
+  const handleTabClick = (userId: number) => {
+    setViewedUserId(userId);
+  };
 
-    const handleTabClick = (userId: number) => {
-        setViewedUserId(userId);
-    };
+  const ids = useMemo(
+    () => [
+      user!.id,
+      ...user!.friends
+        .filter((friend: Friend) => friend.visible)
+        .map((friend: Friend) => friend.id),
+    ],
+    [user]
+  ); // A list containing the user and all the friend ids
 
-    const ids = useMemo(() => [
-        user!.id,
-        ...user!.friends.filter((friend: Friend) => friend.visible).map((friend: Friend) => friend.id),
-    ], [user]);
+  const Tab = ({ id, name }: { id: number; name: string }) => {
+    const isActive = viewedUserId === id;
+    const isSelf = id === user!.id;
+    let icon = null;
 
-    const Tab = ({ id, name }: { id: number, name: string }) => {
-        const isActive = viewedUserId === id;
-        const isSelf = id === user!.id;
+    if (isSelf) {
+      icon = <Trophy className="w-4" />;
+    }
 
-        if (isActive) {
-            return <Button variant="primary" icon={isSelf ? <Trophy className="w-4"/> : null}> {isSelf ? 'You' : name}</Button>;
-        } else {
-            return <Button variant="secondary" className="bg-white text-gray-800" onClick={() => handleTabClick(id)} icon={isSelf ? <Trophy className="w-4"/> : null}> {isSelf ? 'You' : name}</Button>;
-        }
+    if (id === 0) {
+      icon = <Globe className="w-4" />;
+    }
+
+    if (isActive) {
+      return (
+        <Button variant="primary" icon={icon}>
+          {" "}
+          {isSelf ? "You" : name}
+        </Button>
+      );
     }
 
     return (
-        <div className=" bg-gray-200/75 p-1 rounded-lg flex mx-auto mb-2 overflow-x-scroll">
-            <div className="flex mx-auto gap-2 w-fit">
-            {ids.map((id) => {
-                const friend = user!.friends.find((f: Friend) => f.id === id);
-                return (
-                    <Tab
-                        key={id}
-                        id={id}
-                        name={friend ? friend.username : user!.username}
-                    />
-                );
-            })}
-        </div>
+      <Button
+        variant="secondary"
+        className="bg-white text-gray-800"
+        onClick={() => handleTabClick(id)}
+        icon={icon}
+      >
+        {" "}
+        {isSelf ? "You" : name}
+      </Button>
+    );
+  };
 
-        </div>
-    )
+  return (
+    <div className=" bg-gray-200/75 p-1 rounded-lg flex mx-auto mb-2 overflow-x-scroll">
+      <div className="flex mx-auto gap-2 w-fit">
+        <Tab id={0} name="Global" />
+        {ids.map((id) => {
+          const friend = user!.friends.find((f: Friend) => f.id === id);
+          return (
+            <Tab
+              key={id}
+              id={id}
+              name={friend ? friend.username : user!.username}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
