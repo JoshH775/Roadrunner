482867cd3fb727a5eac46934e56da492f80c78af
Joshua Hickey <joshuah775@gmail.com>
2025-07-10 20:55:04 +0100

    Youtube custom icon

--- src/components/UI/CustomIcons.tsx
+++ src/components/UI/CustomIcons.tsx
@@ -2,9 +2,14 @@ type Props = {
   className?: string;
 };
 
-export function LeaderboardIcon({ className = '' }: Props) {
+export function LeaderboardIcon({ className = "" }: Props) {
   return (
-    <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className}>
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
@@ -42,6 +47,26 @@ export function LeaderboardIcon({ className = '' }: Props) {
           strokeLinejoin="round"
         ></path>{" "}
       </g>
+    </svg>
+  );
+}
+
+export function YoutubeIcon({ className = ""}: { className?: string}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={"lucide lucide-youtube-icon lucide-youtube " + className}
+    >
+      <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" />
+      <path d="m10 15 5-3-5-3z" />
     </svg>
   );
 }
